ca1cd4d254b2e2fe2efee0e5b9265d6612ee3b99
Yasuhiro Usutani <y_usutani@mac.com>
2026-08-30 13:35:05 +0900

    Fix landmarks controller test route name

--- test/controllers/landmarks_controller_test.rb
+++ test/controllers/landmarks_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class LandmarksControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get landmarks_index_url
+    get landmarks_url
     assert_response :success
   end
 end
