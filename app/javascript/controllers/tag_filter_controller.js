11ed952b1437bbd331f1a38741dd24affaa1bf28
Yasuhiro Usutani <y_usutani@mac.com>
2026-08-30 15:34:43 +0900

    Add comments to tag_filter_controller.js

--- app/javascript/controllers/tag_filter_controller.js
+++ app/javascript/controllers/tag_filter_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
+// タグ一覧を検索入力でリアルタイムフィルタリングする
 export default class extends Controller {
   static targets = ["input", "list"]
 
+  // 入力値に一致するタグのみ表示し、非一致タグを非表示にする
   filter() {
+    // list内の<label>を取得
     const query = this.inputTarget.value.toLowerCase()
     const labels = this.listTarget.querySelectorAll("label")
 
